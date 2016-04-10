005aeb589090b82c0356d8cf9a453e36c1ddf285
TrustMe5 <1161789396@qq.com>
2016-04-10 22:11:12 +0800

    r

--- typingsystem/app/static/PageJump.js
+++
@@ -1,7 +0,0 @@
-function skip(){
-pagenum=document.getElementById('pagenum').value;
-self.location='/auth/dailypracticeranklist/page='+pagenum;
-}
-function showclassresult(whichclass){
-    self.location='/auth/dailypracticeresultshowByclass/class='+whichclass.value;
-}
